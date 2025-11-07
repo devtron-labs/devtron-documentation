ccd4896c987e9ed58039d83e577fb282251188f0
ashokdevtron <ashok.nayak@devtron.ai>
2025-11-07 16:00:17 +0530

    gh-pages initiated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.venz.shop',
+  url: 'https://docs-test.devtron.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
