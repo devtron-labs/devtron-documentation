6e52177c833bc5c96069d1c500f49f1bdc593c48
ashokdevtron <ashok.nayak@devtron.ai>
2025-11-08 18:10:58 +0530

    Changed URL in config js and static

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs-test.devtron.ai',
+  url: 'https://docs.devtron.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
